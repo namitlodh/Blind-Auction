3136c443e5175646884321301cd5d888960ce174
Namit Lodh <77287454+namitlodh@users.noreply.github.com>
2023-06-26 01:24:10 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,29 @@
+from replit import clear
+from art import logo
+#HINT: You can call clear() to clear the output in the console.
+print(logo)
+
+bids =  {}
+biding_finish = False
+
+def find_highest_bidder(bidding_record):
+  highest_bid = 0
+  for i in bidding_record:
+    amount = bidding_record[i]
+    if amount > highest_bid:
+      highest_bid = amount
+      winner = i
+  clear()
+  print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+while not biding_finish:
+  name = input("What is your name?: ")
+  price = int(input("What is your bid?: $"))
+  bids[name] = price
+  should_continue = input("Are there any other bidders? Type 'yes' or 'no'. ")
+  if should_continue == "no":
+    biding_finish = True
+    find_highest_bidder(bids)
+  elif should_continue =="yes":
+    clear()
+  
